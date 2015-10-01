ffef10b6af60a1607d4b22430e187da286199c1e
Thomas Franquelin <ostapneko@gmail.com>
2015-10-01 16:20:07 +0100

    Fix test

--- src/test/scala/com/gu/identity/play/CookieBuilderTest.scala
+++ src/test/scala/com/gu/identity/play/CookieBuilderTest.scala
@@ -49,21 +49,21 @@ class CookieBuilderTest extends FreeSpec {
       assert(guuCookie.maxAge.get === (90 * 24 * 60 * 60) +- 5)
       assert(!guuCookie.secure)
       assert(!guuCookie.httpOnly)
-      assert(guuCookie === domain)
+      assert(guuCookie.domain === domain)
 
       assert(scguuCookie.name === "SC_GU_U")
       assert(scguuCookie.value === "sc_gu_u_value")
       assert(scguuCookie.maxAge.get === (90 * 24 * 60 * 60) +- 5)
       assert(scguuCookie.secure)
       assert(scguuCookie.httpOnly)
-      assert(scguuCookie === domain)
+      assert(scguuCookie.domain === domain)
 
       assert(scgulaCookie.name === "SC_GU_LA")
       assert(scgulaCookie.value === "sc_gu_la_value")
       assert(scgulaCookie.maxAge === None)
       assert(scgulaCookie.secure)
       assert(scgulaCookie.httpOnly)
-      assert(scgulaCookie === domain)
+      assert(scgulaCookie.domain === domain)
     }
   }
 }
