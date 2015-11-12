56719ba2b7a15b78e98f9ab50d69371717f6c3b0
Roberto Tyley <roberto.tyley@gmail.com>
2015-11-12 12:31:13 +0000

    Support authentication through Identity Access Tokens as well as cookies
    See also https://github.com/guardian/identity-token-auth-sample
    
    https://trello.com/c/d7jD8CUW/120-members-data-api-enable-access-by-mobile-access-tokens

--- src/main/scala/com/gu/identity/play/AuthenticationService.scala
+++ src/main/scala/com/gu/identity/play/AuthenticationService.scala
@@ -1,24 +1,17 @@
 package com.gu.identity.play
 
-import com.gu.identity.cookie.{IdentityCookieDecoder, IdentityKeys}
+import com.gu.identity.cookie.IdentityKeys
 import play.api.mvc.{Request, RequestHeader}
 
 trait AuthenticationService {
 
   val identityKeys: IdentityKeys
 
-  lazy val cookieDecoder = new IdentityCookieDecoder(identityKeys)
+  val authenticatedIdUserProvider: AuthenticatedIdUser.Provider =
+    AccessCredentials.Cookies.authProvider(identityKeys)
 
-  def authenticatedUserFor[A](request: RequestHeader): Option[AuthenticatedIdUser] = for {
-    scGuU <- request.cookies.get("SC_GU_U")
-    guU <- request.cookies.get("GU_U")
-    minimalSecureUser <- cookieDecoder.getUserDataForScGuU(scGuU.value)
-    guUCookieData <- cookieDecoder.getUserDataForGuU(guU.value)
-    user = guUCookieData.user if user.id == minimalSecureUser.id
-  } yield AuthenticatedIdUser(
-    scGuU.value,
-    IdMinimalUser(user.id, user.publicFields.displayName)
-  )
+  def authenticatedUserFor[A](request: RequestHeader) = authenticatedIdUserProvider(request)
 
   def requestPresentsAuthenticationCredentials(request: Request[_]) = authenticatedUserFor(request).isDefined
+
 }
