c779a161531af56b97fcbfe637f1ef21c1769878
Roberto Tyley <roberto.tyley@gmail.com>
2016-07-20 17:59:45 +0100

    WIP: Updates a go-go

--- project/Dependencies.scala
+++ project/Dependencies.scala
@@ -2,10 +2,10 @@ import sbt._
 
 object Dependencies {
 
-  val play = "com.typesafe.play" %% "play" % "2.4.6"
+  val play = "com.typesafe.play" %% "play" % "2.5.4"
   val identityCookie = "com.gu.identity" %% "identity-cookie" % "3.51"
   val identityTestUsers = "com.gu" %% "identity-test-users" % "0.5"
-  val scalaTestPlus = "org.scalatestplus" %% "play" % "1.4.0-M4"
-  val scalactic = "org.scalactic" %% "scalactic" % "2.2.4"
+  val scalaTestPlus = "org.scalatestplus" %% "play" % "1.4.0"
+  val scalactic = "org.scalactic" %% "scalactic" % "2.2.6"
 
 }
