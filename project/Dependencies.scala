e9d0e099ff7ddfebb5276b78fd49c1d6cf224927
Roberto Tyley <roberto.tyley@gmail.com>
2017-02-23 11:12:32 +0000

    ...because membership-frontend still uses it

--- project/Dependencies.scala
+++ project/Dependencies.scala
@@ -2,7 +2,7 @@ import sbt._
 
 object Dependencies {
 
-  val play = "com.typesafe.play" %% "play" % "2.5.12"
+  val play = "com.typesafe.play" %% "play" % "2.4.10"
   val identityCookie = "com.gu.identity" %% "identity-cookie" % "3.78"
   val identityTestUsers = "com.gu" %% "identity-test-users" % "0.6"
   val scalaTestPlus = "org.scalatestplus" %% "play" % "1.4.0"
